01849679017da750c5d32a528e3a5d842992b0f2
Maria Ortiz-Lopez <OrtizLopez.Maria030@gmail.com>
2021-10-06 13:25:47 -0700

    Added deleteByID test, controller, model

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -158,4 +158,51 @@ describe('TOT2-BackEnd routes', () => {
 
     expect(res.body).toEqual([user1, user2]);
   });
+
+  it('deletes an existing user by id via DELETE', async () => {
+    const heroOject = {
+      hp: 5,
+      stm: 5,
+      ac: 5,
+      spt: 5,
+      atk: 5,
+      level: 5,
+      gold: 5,
+      xp: 5,
+    };
+
+    const heldGearObject = {
+      id: 1,
+      name: 'item',
+      price: 5,
+      hp: 0,
+      ac: 0,
+      spd: 0,
+      atk: 0,
+    };
+
+    const itemObject = {
+      id: 1,
+      name: 'item',
+      effect: 'function',
+    };
+
+    const userObject = {
+      googleId: 'googleid',
+      score: 0,
+      heroStats: heroOject,
+      heldGear: [heldGearObject],
+      items: [itemObject],
+      achievements: ['string'],
+      location: 1,
+    };
+    const user = await userModel.insert(userObject);
+
+    const res = await request(app)
+      .delete(`/api/v1/users/${user.googleId}`);
+
+    expect(res.body).toEqual({
+      message: 'This user has retired.'
+    });
+  });
 });
