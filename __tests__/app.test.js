543ac365f57fd94dbba9b7cada8ea13f6b8afa19
Maria Ortiz-Lopez <OrtizLopez.Maria030@gmail.com>
2021-10-06 12:04:32 -0700

    Added in getById test, controller, and Model

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool.js');
 const setup = require('../data/setup.js');
 const request = require('supertest');
 const app = require('../lib/app.js');
+const userModel = require('../lib/models/User.js');
 
 describe('TOT2-BackEnd routes', () => {
   beforeEach(() => {
@@ -56,5 +57,48 @@ describe('TOT2-BackEnd routes', () => {
       id: '1',
       ...userObject,
     });
+  });
+
+  it('gets a user by id via GET', async () => {
+    const heroOject = {
+      hp: 5,
+      stm: 5,
+      ac: 5,
+      spt: 5,
+      atk: 5,
+      level: 5,
+      gold: 5,
+      xp: 5,
+    };
+
+    const heldGearObject = {
+      id: 1,
+      name: 'item',
+      price: 5,
+      hp: 0,
+      ac: 0,
+      spd: 0,
+      atk: 0,
+    };
+
+    const itemObject = {
+      id: 1,
+      name: 'item',
+      effect: 'function',
+    };
+
+    const userObject = {
+      googleId: 'googleid',
+      score: 0,
+      heroStats: heroOject,
+      heldGear: [heldGearObject],
+      items: [itemObject],
+      achievements: ['string'],
+      location: 1,
+    };
+    const user = await userModel.insert(userObject);
+    const res = await request(app).get(`/api/v1/users/${user.googleId}`);
+
+    expect(res.body).toEqual(user);
   });
 });
