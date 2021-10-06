addd1d8bb3b0d600a2fb60a0d471f4b1108e687c
Missael Ortiz <ortiz.missael80@gmail.com>
2021-10-06 10:00:13 -0700

    added setup sql file, init table users

---
+++ sql/setup.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS user;
+
+CREATE TABLE users (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  google_id TEXT NOT NULL,
+  score INT,
+  hero_stats JSON,
+  held_gear JSON[],
+  items JSON[],
+  achievements TEXT[],
+  location INT,
+);
