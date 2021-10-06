f038910001d107635ab65343984b6cfc9581583b
chenerychen <60025491+chenerychen@users.noreply.github.com>
2021-10-06 13:48:56 -0400

    created a user oject and test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -13,6 +13,48 @@ describe('TOT2-BackEnd routes', () => {
   });
 
   it('creates a user via POST', async () => {
-    
+    heroOject = {
+      hp: 5,
+      stm: 5,
+      ac: 5,
+      spt: 5,
+      atk: 5,
+      level: 5,
+      gold: 5,
+      xp: 5,
+    };
+
+    heldGearObject = {
+      id: 1,
+      name: 'item',
+      price: 5,
+      hp: 0,
+      ac: 0,
+      spd: 0,
+      atk: 0,
+    }; 
+
+    itemObject = {
+      id: 1, 
+      name: 'item', 
+      effect: 'function', 
+    }
+
+    const userObject = {
+      google_id: 'googleid',
+      score: 0,
+      hero_stats: heroOject,
+      held_gear: [heldGearObject],
+      items: [itemObject],
+      achievements: ['string'],
+      location: 1,
+    };
+
+    const res = await request(app).post('/api/v1/users').send(userObject); 
+
+    expect(res.body).toEqual({
+      id: '1', 
+      ...userObject, 
+    })
   });
 });
